28789d49d5d7320d705f34d1bd4cbc99e97e4480
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-03-13 01:16:52 +0100

    auth config

---
+++ src/auth/authProvider.tsx
@@ -0,0 +1,21 @@
+// Este va a ser un componente que va a usar useContexxt para guardar todo el estado y las funciones que vamos a necesitar a lo largo de toda nuestra aplicacion
+// Este sera nuestro componente que va a estar validando constantement que exista autenticacion o no para dejar pasar a las rutas que estan protegidas y en general poder acceder a la informacion de nuestro usuario, que mandemos desde el backend
+import { useContext, createContext, useState, useEffect } from "react";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthContext = createContext({
+  isAuthenticated: false,
+});
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./css/App.css";
+import "./css/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./routes/Login.tsx";
 import Signup from "./routes/Signup.tsx";
 import Dashboard from "./routes/Dashboard.tsx";
+import ProtectedRoute from "./routes/ProtectedRoute.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/signup", element: <Signup /> },
-  { path: "/dashboard", element: <Dashboard /> },
+  { path: "/dashboard", element: <ProtectedRoute />, children: [
+    {
+      path: "/dashboard",
+      element: <Dashboard />
+    }
+  ] },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
