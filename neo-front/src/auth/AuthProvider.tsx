4d4b25be64ae186bea799028249d5936c26d2828
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-03-22 01:44:40 +0100

    updt

--- neo-front/src/auth/AuthProvider.tsx
+++ neo-front/src/auth/AuthProvider.tsx
@@ -1,5 +1,5 @@
 import { useContext, createContext, useState, useEffect } from "react";
-import { AuthResponse, AccessTokenResponse } from "src/types/types";
+import type { AuthResponse, AccessTokenResponse, User } from "src/types/types";
 import { API_URL } from "./constants";
 
 interface AuthProviderProps {
@@ -16,68 +16,115 @@ const AuthContext = createContext({
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [accessToken, setAccessToken ] = useState<string>("");
+  const [accessToken, setAccessToken] = useState<string>("");
   const [refreshToken, setRefreshToken] = useState<string>("");
+  const [user, setUser] = useState<User>();
 
   useEffect(() => {}, []);
 
-  async function requestNewAccessToken(refreshToken:string) {
+  async function requestNewAccessToken(refreshToken: string) {
     try {
-      const response = await fetch(`${API_URL}/refresh-token`,{
+      const response = await fetch(`${API_URL}/refresh-token`, {
         method: "POST",
-        headers: { 
+        headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${refreshToken}`,
-        }
-      })
+        },
+      });
 
       if (response.ok) {
-        const json = await response.json() as AccessTokenResponse 
-        if(json.error) {
+        const json = (await response.json()) as AccessTokenResponse;
+        if (json.error) {
           throw new Error(json.error);
         }
         return json.body.accessToken;
+      } else {
+        throw new Error(response.statusText);
       }
     } catch (error) {
       console.log(error);
+      return null;
     }
+  }
 
+  async function getUserInfo(accessToken: string) {
+    try {
+      const response = await fetch(`${API_URL}/user`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (response.ok) {
+        const json = await response.json();
+        if (json.error) {
+          throw new Error(json.error);
+        }
+        return json;
+      } else {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
     }
-  
-  
+  }
+
   async function checkAuth() {
     if (accessToken) {
-      
     } else {
       const token = getRefreshToken();
-      if(token){
-
+      if (token) {
+        const newAccessToken = await requestNewAccessToken(token);
+        if (newAccessToken) {
+          const userInfo = await getUserInfo(newAccessToken);
+          if (userInfo) {
+            saveSessionInfo(userInfo, newAccessToken, token)
+          }
+        }
       }
     }
+  }
+
+  function saveSessionInfo(
+    userInfo: User,
+    accesToken: string,
+    refreshToken: string
+  ) {
+    setAccessToken(accessToken);
+    // setUser(userData.body.user)
+    // setRefreshToken(userData.body.refreshToken);
+    localStorage.setItem("Token", JSON.stringify(refreshToken));
+    setIsAuthenticated(true);
+    setUser(userInfo);
   }
 
   function getAccessToken() {
     return accessToken;
   }
-  function getRefreshToken():string|null {
-    const token = localStorage.getItem("token")
+  function getRefreshToken(): string | null {
+    const token = localStorage.getItem("token");
     if (token) {
-      const {refreshToken} = JSON.parse(token)
+      const { refreshToken } = JSON.parse(token);
       return refreshToken;
     }
     return null;
   }
 
-  function saveUser(userData:AuthResponse) {
-    setAccessToken(userData.body.accessToken);
-    setRefreshToken(userData.body.refreshToken);
-
-    localStorage.setItem("Token", JSON.stringify(userData.body.refreshToken));
-    setIsAuthenticated(true);
+  function saveUser(userData: AuthResponse) {
+    saveSessionInfo(
+      userData.body.user,
+      userData.body.accessToken,
+      userData.body.refreshToken
+    );
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, getAccessToken, saveUser, getRefreshToken }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, getAccessToken, saveUser, getRefreshToken }}
+    >
       {children}
     </AuthContext.Provider>
   );
